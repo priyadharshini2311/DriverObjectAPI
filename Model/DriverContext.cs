41615cbea447412d01e44705a143d816204e51b1
Priyadharshini Nagarajan <pnagarajan@ups.com>
2022-11-21 20:38:16 -0500

    Add project files.

---
+++ Model/DriverContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DriverObject.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DriverObject.Model
+{
+    public class DriverObjectContext : DbContext
+    {
+        public DriverObjectContext(DbContextOptions<DriverObjectContext> options)
+           : base(options)
+        {
+        }
+        public DbSet<DriverObject.Model.Driver> Drivers { get; set; }
+    }
+}
